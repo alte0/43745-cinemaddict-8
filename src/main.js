5e2bcffa5376e594310ad87df046839dbcb1cf95
m.dmitriev <makc-dmv@yandex.ru>
2019-03-08 18:07:10 +0500

    small refactor code

--- src/main.js
+++ src/main.js
@@ -1,16 +1,28 @@
-import {renderData, renderCardsDatafromClass, randomOrderInArrayAndSplice, clearChildEl, deleteEl} from './modules/util';
-import getFilter from './modules/make-filter';
-import {filters, cards} from './modules/data';
-import Card from './clases/card';
-import CardExtra from './clases/card-extra';
-import PopapCard from './clases/popup-card';
+import {
+  renderData,
+  renderCardsDatafromClass,
+  randomOrderInArrayAndSplice,
+  clearChildEl,
+  deleteEl
+} from "./modules/util";
+import getFilter from "./modules/make-filter";
+import {filters, cards} from "./modules/data";
+import Card from "./clases/card";
+import CardExtra from "./clases/card-extra";
+import PopapCard from "./clases/popup-card";
 
-const BODY = document.body;
-const MAIN_NAV = BODY.querySelector(`.main-navigation`);
-const FILMS_CARDS_CONTAINER = BODY.querySelector(`.films .films-list .films-list__container`);
-const FILMS_CARDS_EXTRAS = BODY.querySelectorAll(`.films .films-list--extra`);
-const FILMS_CARDS_EXTRA_TOP = FILMS_CARDS_EXTRAS[0].querySelector(`.films-list__container`);
-const FILMS_CARDS_EXTRA_MOST = FILMS_CARDS_EXTRAS[1].querySelector(`.films-list__container`);
+const body = document.body;
+const mainNav = body.querySelector(`.main-navigation`);
+const filmmsCardsContainer = body.querySelector(
+    `.films .films-list .films-list__container`
+);
+const filmmsCardsContainerExtras = body.querySelectorAll(`.films .films-list--extra`);
+const filmmsCardsContainerExtraTop = filmmsCardsContainerExtras[0].querySelector(
+    `.films-list__container`
+);
+const filmmsCardsContainerExtraMost = filmmsCardsContainerExtras[1].querySelector(
+    `.films-list__container`
+);
 
 const renderFilters = renderData;
 
@@ -18,21 +30,26 @@ const navClickHandler = (evt) => {
   const target = evt.target;
 
   if (target.tagName === `A`) {
-    const NAV_ITEMS = MAIN_NAV.querySelectorAll(`.main-navigation__item`);
+    const navItems = mainNav.querySelectorAll(`.main-navigation__item`);
     const href = target.getAttribute(`href`);
 
-    for (const nav of NAV_ITEMS) {
+    for (const nav of navItems) {
       nav.classList.remove(`main-navigation__item--active`);
     }
     target.classList.add(`main-navigation__item--active`);
-    clearChildEl(FILMS_CARDS_CONTAINER);
+    clearChildEl(filmmsCardsContainer);
 
     if (href === `#all`) {
-      renderCardsDatafromClass(cards, FILMS_CARDS_CONTAINER, Card, detailOpen);
+      renderCardsDatafromClass(cards, filmmsCardsContainer, Card, detailOpen);
     } else if (href === `#stats`) {
       return;
     } else {
-      renderCardsDatafromClass(randomOrderInArrayAndSplice(cards), FILMS_CARDS_CONTAINER, Card, detailOpen);
+      renderCardsDatafromClass(
+          randomOrderInArrayAndSplice(cards),
+          filmmsCardsContainer,
+          Card,
+          detailOpen
+      );
     }
   } else {
     evt.preventDefault();
@@ -40,20 +57,30 @@ const navClickHandler = (evt) => {
 };
 
 const popupClose = () => {
-  deleteEl(BODY, BODY.querySelector(`.film-details`));
+  deleteEl(body, body.querySelector(`.film-details`));
 };
 
 const detailOpen = (dataPopup) => {
   if (!document.body.querySelector(`.film-details`)) {
     const popapCard = new PopapCard(dataPopup);
-    popapCard.render(BODY);
+    popapCard.render(body);
     popapCard.popupClose = popupClose;
   }
 };
 
-renderFilters(filters, MAIN_NAV, getFilter);
-renderCardsDatafromClass(cards, FILMS_CARDS_CONTAINER, Card, detailOpen);
-renderCardsDatafromClass(randomOrderInArrayAndSplice(cards, true), FILMS_CARDS_EXTRA_TOP, CardExtra, detailOpen);
-renderCardsDatafromClass(randomOrderInArrayAndSplice(cards, true), FILMS_CARDS_EXTRA_MOST, CardExtra, detailOpen);
+renderFilters(filters, mainNav, getFilter);
+renderCardsDatafromClass(cards, filmmsCardsContainer, Card, detailOpen);
+renderCardsDatafromClass(
+    randomOrderInArrayAndSplice(cards, true),
+    filmmsCardsContainerExtraTop,
+    CardExtra,
+    detailOpen
+);
+renderCardsDatafromClass(
+    randomOrderInArrayAndSplice(cards, true),
+    filmmsCardsContainerExtraMost,
+    CardExtra,
+    detailOpen
+);
 
-MAIN_NAV.addEventListener(`click`, navClickHandler);
+mainNav.addEventListener(`click`, navClickHandler);
